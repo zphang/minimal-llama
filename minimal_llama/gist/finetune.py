95097445d0832ade19646929829c11826df35e94
Jason Phang <jasonphang@nyu.edu>
2023-05-05 02:57:02 -0400

    s3

--- minimal_llama/gist/finetune.py
+++ minimal_llama/gist/finetune.py
@@ -81,7 +81,8 @@ class ModifiedTrainer(Trainer):
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
         # If we are executing this function, we are the process zero, so we don't check for that.
         output_dir = output_dir if output_dir is not None else self.args.output_dir
-        os.makedirs(output_dir, exist_ok=True)
+        if not output_dir.startswith("s3:"):
+            os.makedirs(output_dir, exist_ok=True)
         io_utils.fsspec_torch_save(
             self.model.state_dict(),
             os.path.join(output_dir, f"checkpoint.p"),
