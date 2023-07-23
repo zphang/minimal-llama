ac2da9e344a00b8dfab8fee563df5502d94d7652
Jason Phang <jasonphang@nyu.edu>
2023-07-22 23:05:46 -0700

    checkin?

--- minimal_llama/hyper/finetune_peft_acc.py
+++ minimal_llama/hyper/finetune_peft_acc.py
@@ -5,9 +5,11 @@ import torch
 import torch.nn.functional as F
 from torch.utils.data.distributed import DistributedSampler
 import minimal_llama.utils.io_utils as io_utils
-import datasets
 import bitsandbytes.optim
 import wandb
+import accelerate
+import datasets
+import datasets.distributed
 
 import minimal_llama.hyper.prefix_llama as prefix_llama
 import minimal_llama.hyper.prefix_makers as prefix_makers
@@ -35,16 +37,29 @@ def run():
     parser.add_argument("--run_name", type=str, default=None)
     parser.add_argument("--model_size", type=str, default="7b")
     parser.add_argument("--lr_scheduler", action="store_true")
+    parser.add_argument("--no_wandb", action="store_true", default=False)
     args = parser.parse_args()
     assert args.prefix_maker_mode in ["plain", "mlp", "hidden_states"]
+    torch.manual_seed(0)
 
-    local_rank = int(os.environ.get('LOCAL_RANK', 0))
-    rank = int(os.environ.get('RANK', 0))
-    world_size = int(os.environ.get('WORLD_SIZE', 1))
-    torch.cuda.set_device(local_rank)
-    device = torch.device("cuda", local_rank)
+    # Initialize Accelerate
+    accelerator = accelerate.Accelerator(
+        mixed_precision="bf16",
+        # mixed_precision="no",
+        dispatch_batches=False,
+        gradient_accumulation_steps=args.grad_accum_steps,
+    )
+    device = accelerator.device
+    print0 = accelerator.on_local_main_process(print)
+    use_wandb = not args.no_wandb and accelerator.is_main_process
 
-    if rank == 0:
+    # local_rank = int(os.environ.get('LOCAL_RANK', 0))
+    # rank = int(os.environ.get('RANK', 0))
+    # world_size = int(os.environ.get('WORLD_SIZE', 1))
+    # torch.cuda.set_device(local_rank)
+    # device = torch.device("cuda", local_rank)
+
+    if use_wandb:
         wandb.init(
             name=args.run_name,
             project="hyper2",
@@ -79,7 +94,9 @@ def run():
             prefix_type=args.prefix_maker_mode,
             include_gates=args.prefix_include_gates,
         ).to(device)
-        optimizer = bitsandbytes.optim.AdamW(prefix_maker.parameters(), lr=args.lr, is_paged=True, optim_bits=32)
+        # optimizer = bitsandbytes.optim.AdamW(prefix_maker.parameters(), lr=args.lr, is_paged=True, optim_bits=32)
+        optimizer = torch.optim.Adam(prefix_maker.parameters(), lr=args.lr, betas=(0.9, 0.99))
+        save_model = prefix_maker
     elif args.peft_type == "lora":
         config = lora_llama.LLAMA_CONFIG_DICT[args.model_size]
         config.dtype = torch.bfloat16
@@ -90,8 +107,11 @@ def run():
             config=config,
             hf_path=args.hf_path,
             use_4bit=True,
+            device=device,
         )
-        optimizer = bitsandbytes.optim.AdamW(model.parameters(), lr=args.lr, is_paged=True, optim_bits=32)
+        # optimizer = bitsandbytes.optim.AdamW(model.parameters(), lr=args.lr, is_paged=True, optim_bits=32)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.99))
+        save_model = model
     else:
         raise KeyError(args.peft_type)
 
@@ -104,14 +124,17 @@ def run():
     else:
         scheduler = None
 
+    print(f"Rank: {accelerator.process_index}", model.model.layers[0].self_attn.q_proj.lora_a.mean())
+
     # Loading
     if os.path.exists(os.path.join(args.save_dir, "train_state.json")):
         train_state = io_utils.read_json(os.path.join(args.save_dir, "train_state.json"))
         completed_steps = train_state["completed_steps"]
         load_path = os.path.join(args.save_dir, f"checkpoint_{completed_steps:05d}.pt")
-        print("Resuming from", load_path)
+        print0("Resuming from", load_path)
         loaded = torch.load(load_path)
         if args.peft_type == "prefix":
+            # noinspection PyUnboundLocalVariable
             prefix_maker.load_state_dict(loaded["model"])
         elif args.peft_type == "lora":
             model.load_state_dict(loaded["model"], strict=False)
@@ -120,86 +143,85 @@ def run():
         optimizer.load_state_dict(loaded["optimizer"])
         if scheduler is not None:
             scheduler.load_state_dict(loaded["scheduler"])
-        loss_list = io_utils.read_json(os.path.join(args.save_dir, f"loss_{completed_steps:05d}.json"))
     else:
         train_state = {"completed_steps": 0}
         os.makedirs(args.save_dir, exist_ok=True)
-        loss_list = []
+
+    if scheduler:
+        # noinspection PyTypeChecker
+        model, optimizer, scheduler = accelerator.prepare(model, optimizer, scheduler)
+    else:
+        # noinspection PyTypeChecker
+        model, optimizer = accelerator.prepare(model, optimizer)
 
     ds = datasets.load_from_disk(args.dataset_path)
     train_iterator = get_train_iterator(
         ds,
-        rank=rank, world_size=world_size,
+        rank=accelerator.process_index, world_size=accelerator.num_processes,
         batch_size=args.batch_size, num_workers=args.num_workers,
         total_steps=args.total_steps,
         start_step=train_state["completed_steps"],
         seed=train_state["completed_steps"],  # use steps as stand-in for seed
         grad_accum_steps=args.grad_accum_steps,
     )
+    torch.cuda.empty_cache()
+    accelerator.wait_for_everyone()
+
     loss = None
     optimizer.zero_grad()
     for batch_metadata, batch in train_iterator:
-        if args.peft_type == "prefix":
-            prefixes = prefix_maker(batch_size=batch["input_ids"].shape[0])
-            output = model(
-                input_ids=batch["input_ids"][:, :-1].to(device),
-                prefixes=prefixes,
+        with accelerator.accumulate(model):
+            if args.peft_type == "prefix":
+                prefixes = prefix_maker(batch_size=batch["input_ids"].shape[0])
+                output = model(
+                    input_ids=batch["input_ids"][:, :-1].to(device),
+                    prefixes=prefixes,
+                )
+            elif args.peft_type == "lora":
+                output = model(
+                    input_ids=batch["input_ids"][:, :-1].to(device),
+                    use_pefts=True,
+                )
+            else:
+                raise KeyError(args.peft_type)
+            loss = F.cross_entropy(
+                output.view(-1, output.size(-1)),
+                batch["labels"][:, 1:].reshape(-1).to(device),
             )
-        elif args.peft_type == "lora":
-            output = model(
-                input_ids=batch["input_ids"][:, :-1].to(device),
-                use_pefts=True,
-            )
-        else:
-            raise KeyError(args.peft_type)
-        loss = F.cross_entropy(
-            output.view(-1, output.size(-1)),
-            batch["labels"][:, 1:].reshape(-1).to(device),
-        )
-        if rank == 0:
-            wandb.log({"loss": loss.item(), "step": batch_metadata["curr_step"]})
-        loss.backward()
-        if batch_metadata["grad_accum_index"] == args.grad_accum_steps - 1:
+            print(f"Rank: {accelerator.process_index}, Loss: {loss}, Step: {batch_metadata['curr_step']}")
+            if torch.isnan(loss):
+                raise RuntimeError(f"NaN on rank {accelerator.process_index}")
+            accelerator.backward(loss)
             optimizer.step()
             optimizer.zero_grad()
             if scheduler:
                 scheduler.step()
-            if local_rank == 0:
-                print(batch_metadata["curr_step"], "Mem:", torch.cuda.max_memory_allocated(device), loss.item())
-                loss_list.append(loss.item())
+        if use_wandb:
+            global_loss = accelerator.reduce(loss, "mean")
+            wandb.log({"loss": global_loss.item(), "step": batch_metadata["curr_step"]})
+        if batch_metadata["grad_accum_index"] == args.grad_accum_steps - 1:
+            print0(batch_metadata["curr_step"], "Mem:", torch.cuda.max_memory_allocated(device), loss.item())
         completed_steps = batch_metadata["curr_step"] + 1
         if completed_steps % args.save_freq == 0:
-            if args.peft_type == "prefix":
-                model_state_dict = prefix_maker.state_dict()
-            elif args.peft_type == "lora":
-                model_state_dict = torch_utils.get_requires_grad(model)
-                if not model_state_dict:
-                    raise RuntimeError()
-            else:
-                raise KeyError(args.peft_type)
-            torch.save({
-                "model": model_state_dict,
-                "optimizer": optimizer.state_dict(),
-                "scheduler": scheduler.state_dict() if scheduler else None,
-            }, os.path.join(args.save_dir, f"checkpoint_{completed_steps:05d}.pt"))
-            io_utils.write_json(loss_list, os.path.join(args.save_dir, f"loss_{completed_steps:05d}.json"))
-            io_utils.write_json({"completed_steps": completed_steps}, os.path.join(args.save_dir, f"train_state.json"))
+            save_checkpoint(
+                accelerator=accelerator,
+                save_model=save_model,
+                optimizer=optimizer,
+                scheduler=scheduler,
+                save_dir=args.save_dir,
+                completed_steps=completed_steps,
+            )
 
-    if args.peft_type == "prefix":
-        model_state_dict = prefix_maker.state_dict()
-    elif args.peft_type == "lora":
-        model_state_dict = torch_utils.get_requires_grad(model)
-    else:
-        raise KeyError(args.peft_type)
-
-    torch.save({
-        "model": model_state_dict,
-    }, os.path.join(args.save_dir, "checkpoint_final.pt"))
-    io_utils.write_json(loss_list, os.path.join(args.save_dir, "loss_final.json"))
-
-    if local_rank == 0:
-        print("Mem:", torch.cuda.max_memory_allocated(device))
-        print("Done", loss.item())
+    save_checkpoint(
+        accelerator=accelerator,
+        save_model=save_model,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        save_dir=args.save_dir,
+        completed_steps=args.total_steps,
+    )
+    print0("Mem:", torch.cuda.max_memory_allocated(device))
+    print0("Done", loss.item())
 
 
 def data_collator(features: list) -> dict:
@@ -219,7 +241,7 @@ def get_train_iterator(dataset,
                        batch_size: int, num_workers: int,
                        total_steps: int,
                        start_step: int = 0, grad_accum_steps: int = 1,
-                       seed: int  = 0):
+                       seed: int = 0):
     total_micro_steps = total_steps * grad_accum_steps
     start_micro_step = start_step * grad_accum_steps
     sampler = DistributedSampler(
@@ -267,6 +289,22 @@ def get_train_iterator(dataset,
             curr_micro_step += 1
             if curr_micro_step == total_micro_steps:
                 return
+
+
+def save_checkpoint(accelerator, save_model, optimizer, scheduler, save_dir, completed_steps):
+    accelerator.wait_for_everyone()
+    if accelerator.is_main_process:
+        unwrapped_model = accelerator.unwrap_model(save_model)
+        model_state_dict = torch_utils.get_requires_grad(unwrapped_model)
+        torch.save({
+            "model": model_state_dict,
+            "optimizer": optimizer.state_dict(),
+            "scheduler": scheduler.state_dict() if scheduler else None,
+        }, os.path.join(save_dir, f"checkpoint_{completed_steps:05d}.pt"))
+        io_utils.write_json({
+            "completed_steps": completed_steps,
+        }, os.path.join(save_dir, f"train_state.json"))
+    accelerator.wait_for_everyone()
 
 
 if __name__ == "__main__":
